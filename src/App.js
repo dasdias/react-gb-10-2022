03376ad44c3891bfb89f3f928fda585202332559
Alex Fix <dasdias@yandex.ru>
2022-11-15 09:35:32 +0300

    lesson-04

--- src/App.js
+++ src/App.js
@@ -7,58 +7,58 @@ import { MainPage } from './components/';
 import { Profile } from './components/';
 
 export function App() {
-  const [inputMsg, setInputMsg] = useState({
-    text: "",
-    author: "",
-  });
-  const [messageList, setMessageList] = useState([]);
-  const [chatList, setChatList] = useState([
-    { id: "chat01", name: "Chat-01" },
-    { id: "chat02", name: "Chat-02" },
-    { id: "chat02", name: "Chat-03" },
-  ]);
+	const [inputMsg, setInputMsg] = useState({
+		text: "",
+		author: "",
+	});
+	const [messageList, setMessageList] = useState([]);
+	// const [chatList, setChatList] = useState([
+	//   { id: "chat01", name: "Chat-01" },
+	//   { id: "chat02", name: "Chat-02" },
+	//   { id: "chat02", name: "Chat-03" },
+	// ]);
 
-  const robotMsg = "Мы получили ваше сообщение и скоро свяжемся с вами.";
+	const robotMsg = "Мы получили ваше сообщение и скоро свяжемся с вами.";
 
 
-  useEffect(() => {
-    let timerId;
-    if (messageList.length > 0 && messageList[messageList.length - 1].author !== 'robot') {
-      timerId = setTimeout(() => {
-        setMessageList((prev) => [...prev, { text: robotMsg, author: 'robot' }])
-      }, 1500)
-    }
+	useEffect(() => {
+		let timerId;
+		if (messageList.length > 0 && messageList[messageList.length - 1].author !== 'robot') {
+			timerId = setTimeout(() => {
+				setMessageList((prev) => [...prev, { text: robotMsg, author: 'robot' }])
+			}, 1500)
+		}
 
-    return (() => {
-      clearTimeout(timerId);
-    })
+		return (() => {
+			clearTimeout(timerId);
+		})
 
-  }, [messageList])
-  return (
-    <BrowserRouter>
-      <div className="app">
-        <Menu />
-        <div className="body">
-          <Routes>
-            <Route path="/" element={<MainPage></MainPage>} />
-            <Route path="/chats"
-              element={
-                <PageChats 
-                  messages={messageList}
-                  chatList={chatList}
-                  setChatList={setChatList}
-                  dataInput={inputMsg}
-                  setInputMsg={setInputMsg}
-                  setMessage={setMessageList} />
-              }
-            />
-            <Route path="/profile" element={<Profile/>} />
-            <Route path="*" element={<div> <h2 style={{"textAlign": "center"}}>404 Page</h2></div>} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter >
-  );
+	}, [messageList])
+	return (
+		<BrowserRouter>
+			<div className="app">
+				<Menu />
+				<div className="body">
+					<Routes>
+						<Route path="/" element={<MainPage></MainPage>} />
+						<Route path="/chats"
+							element={
+								<PageChats
+									messages={messageList}
+									// chatList={chatList}
+									// setChatList={setChatList}
+									dataInput={inputMsg}
+									setInputMsg={setInputMsg}
+									setMessage={setMessageList} />
+							}
+						/>
+						<Route path="/profile" element={<Profile />} />
+						<Route path="*" element={<div> <h2 style={{ "textAlign": "center" }}>404 Page</h2></div>} />
+					</Routes>
+				</div>
+			</div>
+		</BrowserRouter >
+	);
 }
 
 // export default App;
--- src/components/chatList/chatList.jsx
+++ src/components/chatList/chatList.jsx
@@ -1,8 +1,17 @@
 import { IconButton, List, ListItem, ListItemText } from "@mui/material";
 import style from "./chatlist.module.css";
 import CommentIcon from '@mui/icons-material/Comment';
+import { useState } from "react";
 
-export const ChatList = ({ chatList, setChatList }) => {
+
+
+export const ChatList = () => {
+	const [chatList, setChatList] = useState([
+		{ id: "chat01", name: "Chat-01" },
+		{ id: "chat02", name: "Chat-02" },
+		{ id: "chat02", name: "Chat-03" },
+	]);
+
 	return (
 
 		<div className={[style.chatlist, style.chatlist_shadow].join(' ')}>
